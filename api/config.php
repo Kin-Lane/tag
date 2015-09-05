00ee8056270e75e030dd505851ec576980638e94
Kin Lane <info@apievangelist.com>
2015-09-05 00:15:54 -0700

    change db

--- api/config.php
+++ api/config.php
@@ -1,8 +1,8 @@
 <?php
 
 $dbserver = "laneworks-2.cjgvjastiugl.us-east-1.rds.amazonaws.com";
-$dbname = "stack_network_kinlane_tagapi";
-//$dbname = "apievangelist";
+//$dbname = "stack_network_kinlane_tagapi";
+$dbname = "apievangelist";
 $dbuser = "kinlane";
 $dbpassword = "ap1stack!";
 
